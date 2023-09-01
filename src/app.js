a01ddfe5b29e469482bae72fce3b548ce93d9a37
omararcx <100163833+omararcx@users.noreply.github.com>
2023-09-01 19:18:13 +0000

    cambios iniciales para excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+
+let action = ['ate', 'peed', 'crushed', 'broke'];
+
+let what = ['my homework', 'the keys', 'the car'];
+
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+let excuse = document.querySelector("#excuse");
+
+window.onload = function() () {
+  let randWho = who[ Math.floor(Math.Random() *who.length)];
+  let randAct = action[ Math.floor(Math.Random() *action.length)];
+  let randWhat = what[ Math.floor(Math.Random() * what.length)];
+  let randWhen = when[ Math.floor(Math.Random() *when.length)];
+  
+  
+  return (`randWho +randAct +randWhat +randWhen`)
+}
