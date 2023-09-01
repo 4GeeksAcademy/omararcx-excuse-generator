b3961d18174d02d81c73d3f87d71015260e56d80
omararcx <100163833+omararcx@users.noreply.github.com>
2023-09-01 20:44:00 +0000

    Ended js file and corrected html and css, tested

--- src/app.js
+++ src/app.js
@@ -5,24 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
 
+let action = ["ate", "peed", "crushed", "broke"];
 
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let what = ["my homework", "the keys", "the car"];
 
-let action = ['ate', 'peed', 'crushed', 'broke'];
-
-let what = ['my homework', 'the keys', 'the car'];
-
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-
-let excuse = document.querySelector("#excuse");
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
 window.onload = function() {
-  let randWho = who[ Math.floor(Math.randomndom() *who.length)];
-  let randAct = action[ Math.floor(Math.random() *action.length)];
-  let randWhat = what[ Math.floor(Math.random() * what.length)];
-  let randWhen = when[ Math.floor(Math.random() *when.length)];
-  
-  
-  return (`randWho +randAct +randWhat +randWhen`)
-}
+  let randWho = Math.floor(Math.random() * who.length);
+  let randAct = Math.floor(Math.random() * action.length);
+  let randWhat = Math.floor(Math.random() * what.length);
+  let randWhen = Math.floor(Math.random() * when.length);
+  let excuse = document.querySelector("#excuse");
+
+  excuse.innerHTML = `${who[randWho]} ${action[randAct]} ${what[randWhat]} ${when[randWhen]}`;
+};
